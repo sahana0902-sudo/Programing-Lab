05494f9fc9f29740c48d08215c5417a049c2c2b6
sahana0902-sudo <nandinic94@gmail.com>
2025-09-01 23:28:39 +0530

    Add files via upload

---
+++ assignment 24.c
@@ -0,0 +1,11 @@
+//wap to find the size of int, float, double and char datatypes
+#include<stdio.h>
+int main()
+{
+	printf("size of int: %zu bytes\n", sizeof(int));
+	printf("size of float: %zu bytes\n", sizeof(float));
+	printf("size of double: %zu bytes\n", sizeof(double));
+	printf("size of char: %zu bytes\n", sizeof(char));
+	return 0;
+	
+}
